563b7e7d26a94140aa2f69e4bf8acd9932526b2b
Nolene Human <108449986+Nolene-Human@users.noreply.github.com>
2024-11-26 15:13:38 +1300

    Update TerminalLogin.py

--- TerminalLogin.py
+++ TerminalLogin.py
@@ -1,6 +1,6 @@
 # Step 1: Basic Python program to check if the password entered is correct or not
 x=3
-password="Hello World"
+password="Password"
 
 # Step 2: Add a loop to allow the user to enter the password again if the password is incorrect, and limit the number of attempts to 3,  and if the user fails to enter the correct password in 3 attempts, the program should terminate and display a message "You have exceeded the number of attempts, please try again later"
 while x>0:
@@ -12,7 +12,7 @@ while x>0:
         print("The credentials provided is incorrect")
         x=x-1
         if x==0:
-            print("You have exceeded the number of attempts, please try again later")
+            print("The password did not match this email address we hold on record")
             break
 
 # Code without 3 attempt limitation
@@ -20,7 +20,7 @@ while x>0:
     print("Welcome back")
 
 else:
-    print("The credentials provided is incorrect") """
+    print("The password did not match this email address we hold on record") """
 
 
 # Future Work: Transfer login to stack
