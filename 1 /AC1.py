1bfc3d06f90c0e5001b9cd7984ae6d9a61a8e74c
Nolene Human <108449986+Nolene-Human@users.noreply.github.com>
2024-12-09 14:49:40 +1300

    Create AC1.py

---
+++ 1 /AC1.py
@@ -0,0 +1,12 @@
+# MVP 1: Pyton login page
+'''**Test Plan:**
+**Action**
+Run the program
+**Expected Result**
+The system should ask the user to enter username
+The system should ask the user to enter password
+The system should ask the user to enter OTP'''
+
+input("Please enter username: ")
+input("Please enter your password: ")
+input("enter otp: ")
